3b60b27ffa27c58708ad920f5d7213182f1bb737
thottanjohn <John Tony>
2020-03-22 12:46:30 +0530

    data retrieved

--- facedata.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-var express = require('express');
-var app = express();
-app.get('/', (req, res) => {
-        var data = JSON.parse(fs.readFileSync( 'FINAL/src/4forces.json'));     
-        res.render("index", {data:data});
-    });
-app.listen(3000, () => console.log(`Server is up on port:3001`));
